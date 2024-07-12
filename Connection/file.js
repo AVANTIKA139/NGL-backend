44e5f9f0362660bf9cf1dc3c692900064e9a3eae
Avantika tiwari <avantikatiwari18@gmail.com>
2024-07-12 11:14:00 +0530

    project completed

--- Connection/file.js
+++ Connection/file.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
+    await mongoose.connect(process.env.MONGODB_URL).then(() => {
       console.log("Database Connected");
     });
   } catch (error) {
